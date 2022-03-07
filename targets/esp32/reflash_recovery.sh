07f9351fcf741e9b40acf7ee58001c4752a86e5e
Michał <michal.b2000@onet.pl>
2022-03-07 14:59:11 +0100

    feat: increase flashing baudrate

--- targets/esp32/reflash_recovery.sh
+++ targets/esp32/reflash_recovery.sh
@@ -4,4 +4,4 @@ cd recovery
 idf.py build
 cd ..
 cp recovery/build/recovery.bin build/recovery.bin
-esptool.py -p $1 -b 460800 --before default_reset --after hard_reset --chip esp32 write_flash --flash_mode dio --flash_size detect --flash_freq 80m 0x90000 build/recovery.bin
+esptool.py -p $1 -b 921600 --before default_reset --after hard_reset --chip esp32 write_flash --flash_mode dio --flash_size detect --flash_freq 80m 0x90000 build/recovery.bin
