d49f05c1c14fced79ed3c95216937b8943a92314
michal4132 <michal.b2000@onet.pl>
2022-04-02 22:44:44 +0000

    fix: revert loadScript path

--- targets/cli/FileScriptLoader.cpp
+++ targets/cli/FileScriptLoader.cpp
@@ -11,14 +11,14 @@ FileScriptLoader::FileScriptLoader() {
 
 void FileScriptLoader::loadScript(std::string scriptName, std::shared_ptr<berry::VmState> berry) {
     BELL_LOG(info, "fileloader", "Loading script: %s ", scriptName.c_str());
-    auto content = loadFile(scriptName);
+    auto content = loadFile("../../../euphonium/scripts/" + scriptName);
     if (!berry->execute_string(content)) {
         EUPH_LOG(error, "script_loader", "Failed to load script %s", scriptName.c_str());
     }
 }
 
 std::string FileScriptLoader::loadFile(std::string fileName) {
-    std::ifstream indexFile("../../../euphonium/scripts/" + fileName);
+    std::ifstream indexFile(fileName);
     std::string indexContent((std::istreambuf_iterator<char>(indexFile)),
             std::istreambuf_iterator<char>());
 
